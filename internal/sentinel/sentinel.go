c73a6c97b05c1fdf393f76f4da34b2ce1190106d
Aleksandr Litreev <aleksandr@litreev.com>
2023-10-23 14:34:01 +0300

    Apply Sentinel API fix

--- internal/sentinel/sentinel.go
+++ internal/sentinel/sentinel.go
@@ -547,7 +547,7 @@ func (s Sentinel) CreateCredentials(nodeAddress string, subscriptionID int64, mn
 	}
 
 	args := fmt.Sprintf(
-		"?rpc_address=%s&chain_id=%s&gas_prices=%s&gas=%d",
+		"?rpc_address=%s&chain_id=%s&gas_prices=%s&gas=%d&simulate_and_execute=false",
 		s.RPCEndpoint,
 		s.ChainID,
 		s.GasPrice+s.DefaultDenom,
