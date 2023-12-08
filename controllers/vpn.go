42bd45321a610644e5d0e556ab81dc4f8549c171
Srinivas Baride <srini@exidio.co>
2023-12-08 22:30:46 +0530

    Fix connecting to same node on retry

--- controllers/vpn.go
+++ controllers/vpn.go
@@ -257,7 +257,7 @@ func (vc VPNController) ConnectToCity(c *gin.Context) {
 	var server models.Server
 
 	if protocol != "" {
-		err = vc.DB.Order("id").First(&server, "current_load < ? AND country_id = ? AND city_id = ? AND is_included_in_plan = ? AND is_banned = ? AND is_active = ? AND protocols->>0 = ?", 0.9, countryId, cityId, true, false, true, protocol).Error
+		err = vc.DB.Order("RANDOM()").First(&server, "current_load < ? AND country_id = ? AND city_id = ? AND is_included_in_plan = ? AND is_banned = ? AND is_active = ? AND protocols->>0 = ?", 0.9, countryId, cityId, true, false, true, protocol).Error
 		if err != nil {
 			middleware.RespondErr(c, middleware.APIErrorUnknown, "failed to get server: "+err.Error())
 			return
@@ -267,7 +267,7 @@ func (vc VPNController) ConnectToCity(c *gin.Context) {
 		return
 	}
 
-	err = vc.DB.Order("id").First(&server, "current_load < ? AND country_id = ? AND city_id = ? AND is_included_in_plan = ? AND is_banned = ? AND is_active = ?", 0.9, countryId, cityId, true, false, true).Error
+	err = vc.DB.Order("RANDOM()").First(&server, "current_load < ? AND country_id = ? AND city_id = ? AND is_included_in_plan = ? AND is_banned = ? AND is_active = ?", 0.9, countryId, cityId, true, false, true).Error
 	if err != nil {
 		middleware.RespondErr(c, middleware.APIErrorUnknown, "failed to get server: "+err.Error())
 		return
